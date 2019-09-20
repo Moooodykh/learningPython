8754616f4bcc25cf05abc606545f639f7703d4f9
Mouhammad <mouhammad.kharboutli@gmail.com>
2019-09-20 15:45:25 +0200

    adding David excersises

---
+++ Youtube learning/variables.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+
+'''
+defining variables
+'''
+
+
+"""
+name = 'John Smith'
+age = 20
+is_newPatient = True
+"""
+
+#input (ask the user to enter a content)
+"""
+userName = input('what is your name? ')
+userColor = input('what is your favorite color? ')
+print(userName +' likes ' + userColor)
+"""
+
+#calculate age 
+"""
+birth_year =int(input('Birth year ? '))
+age = 2019 - birth_year
+print('your age is : ' + str(age)) 
+"""
+
+#Convert pound to Kilograms
+user_pounds = input('Enter your weight in pounds : ')
+calculated_weigt 
